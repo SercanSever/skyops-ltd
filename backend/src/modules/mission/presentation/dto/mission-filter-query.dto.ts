829b3cda4add138ef1513928a954d8c9b5d664ae
SercanSever <sercan.sever16@gmail.com>
2026-04-06 17:34:25 +0300

    refactor(missions): remove unused import of MissionStatus enum from mission filter query DTO

--- backend/src/modules/mission/presentation/dto/mission-filter-query.dto.ts
+++ backend/src/modules/mission/presentation/dto/mission-filter-query.dto.ts
@@ -6,7 +6,6 @@ import {
   IsUUID,
 } from 'class-validator';
 import { PaginationQueryDto } from '../../../../common/dto/pagination-query.dto';
-import { MissionStatus } from '../../domain/enums/mission-status.enum';
 
 export class MissionFilterQueryDto extends PaginationQueryDto {
   @IsOptional()
